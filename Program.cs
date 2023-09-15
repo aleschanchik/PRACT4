c64080d747183d209a208f7358c3445aba4dbaed
aleschanchik <134279317+aleschanchik@users.noreply.github.com>
2023-09-15 11:36:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите ваше имя: "); // Пользователь вводит свое имя
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hello world from " + name); // Выводим сообщение с именем пользователя
+
+        Console.ReadKey();
+    }
+}
